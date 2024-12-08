c10a71e149bb1e8dbe8d9b0339a7c51218629eaf
augustolima1 <62573696+augustolima1@users.noreply.github.com>
2024-12-08 23:58:53 +0000

    configuration migrations + test and endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,20 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -11,19 +25,15 @@ async function query(queryObject) {
   });
 
   await client.connect();
-  try {
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await client.end();
-  }
+  return client;
 }
 
-export default {
-  query: query,
+const database = {
+  query,
+  getNewClient,
 };
+
+export default database;
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
